67273f28a1b8c0c27531f03270c00e84310954e7
MatthewClarkMay <mcmay.web@gmail.com>
2016-02-10 16:28:20 -0500

    cp/mv buddy.vim

--- kali-fresh-setup.sh
+++ kali-fresh-setup.sh
@@ -17,6 +17,30 @@ echo "-------------------------------------------------------------------"
 echo "---------- Lots of cool software installed, Next Phase ------------"
 echo "-------------------------------------------------------------------"
 
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo mv /etc/vim/vimrc /etc/vim/vimrc-backup
+sudo cp vimrc /etc/vim/
+
+echo "-------------------------------------------------------------------"
+echo "------------- Vundle and vimrc configured, Next Phase -------------"
+echo "-------------------------------------------------------------------"
+
+mkdir ~/.vim/colors
+wget https://raw.githubusercontent.com/DrSpatula/vim-buddy/master/colors/buddy.vim
+mv buddy.vim ~/.vim/colors
+
+echo "-------------------------------------------------------------------"
+echo "------------ vim color scheme configured, Next Phase --------------"
+echo "-------------------------------------------------------------------"
+
+sudo mv ~/.bashrc ~/.bashrc-copy
+sudo cp .bashrc ~
+sudo cp .bash_aliases ~
+
+echo "-------------------------------------------------------------------"
+echo "------------ .bashrc copied and replaced, Next Phase --------------"
+echo "-------------------------------------------------------------------"
+
 cd /opt/
 
 if [[ $(getconf LONG_BIT) = "64" ]]
@@ -52,31 +76,6 @@ msfdb init
 
 echo "-------------------------------------------------------------------"
 echo "--------------- Metasploit configured, Next Phase -----------------"
-echo "-------------------------------------------------------------------"
-
-cd ~/kali-setup-scripts        
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-sudo mv /etc/vim/vimrc /etc/vim/vimrc-backup
-sudo cp vimrc /etc/vim/
-
-echo "-------------------------------------------------------------------"
-echo "------------- Vundle and vimrc configured, Next Phase -------------"
-echo "-------------------------------------------------------------------"
-
-mkdir ~.vim/colors
-wget https://raw.githubusercontent.com/DrSpatula/vim-buddy/master/colors/buddy.vim
-mv buddy.vim ~/.vim/colors
-
-echo "-------------------------------------------------------------------"
-echo "------------ vim color scheme configured, Next Phase --------------"
-echo "-------------------------------------------------------------------"
-
-sudo mv ~/.bashrc ~/.bashrc-copy
-sudo cp .bashrc ~
-sudo cp .bash_aliases ~
-
-echo "-------------------------------------------------------------------"
-echo "------------ .bashrc copied and replaced, Next Phase --------------"
 echo "-------------------------------------------------------------------"
 
 
